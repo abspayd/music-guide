da48072f0ab0591a54367a53c5e9c9918667dc19
Adam Spayd <abspayd@mtu.edu>
2023-11-18 01:00:54 -0500

    Update go.mod

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/abspayd/music-companion
 
-go 1.21.2
+go 1.21.4
--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 )
 
 func main() {
-
 	port := 3000
 	fmt.Printf("Starting server on port %d.\n", port)
 	router.HandleRoutes()
