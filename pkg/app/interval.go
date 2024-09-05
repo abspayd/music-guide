b32fdf3c7dedbb889eb3a3aff72028f1cd95f2fe
Adam Spayd <abspayd@mtu.edu>
2024-09-05 11:08:28 -0400

    Adds testing and some functionality to interval finder

--- pkg/app/interval.go
+++ pkg/app/interval.go
@@ -2,29 +2,178 @@ package app
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
-func IntervalDistance(pitch1, pitch2 int) int {
-	return pitch2 - pitch1
-}
-
-func IntervalName(interval_distance int) (string, error) {
-
-	interval_names := map[int]string{
-		0: "Perfect unison",
-		1: "Minor second",
-		2: "Major second",
-		3: "Minor third",
-		4: "Major third",
-		5: "Perfect fourth",
-		6: "Tritone",
-		7: "Perfect fifth",
-		8: "Minor sixth",
-		9: "Major sixth",
+var (
+	// Intervals named by number of steps from starting pitch
+	interval_names = map[int]string{
+		0:  "Perfect unison",
+		1:  "Minor second",
+		2:  "Major second",
+		3:  "Minor third",
+		4:  "Major third",
+		5:  "Perfect fourth",
+		6:  "Tritone",
+		7:  "Perfect fifth",
+		8:  "Minor sixth",
+		9:  "Major sixth",
 		10: "Minor seventh",
 		11: "Major seventh",
 		12: "Perfect octave",
 	}
+
+	// Natural pitches and their index values relative to the 12 pitches
+	naturals = map[rune]int{
+		'C': 0,
+		'D': 2,
+		'E': 4,
+		'F': 5,
+		'G': 7,
+		'A': 9,
+		'B': 11,
+	}
+
+	// All note names, named as sharp
+	sharp_notes = []string{
+		"C",
+		"C#",
+		"D",
+		"D#",
+		"E",
+		"F",
+		"F#",
+		"G",
+		"G#",
+		"A",
+		"A#",
+		"B",
+	}
+
+	// All note names, named as flats
+	flat_notes = []string{
+		"C",
+		"Db",
+		"D",
+		"Eb",
+		"E",
+		"F",
+		"Gb",
+		"G",
+		"Ab",
+		"A",
+		"Bb",
+		"B",
+	}
+)
+
+type Pitch struct {
+	Class  string // A note's name. E.g.: C, C#, Cb, etc.
+	Octave int    // A note's octave register
+}
+
+func (p Pitch) validatePitch() bool {
+	if len(p.Class) == 0 {
+		return false
+	}
+	r := regexp.MustCompile(`^[A-G][b#]$`)
+	if !r.MatchString(p.Class) {
+		return false
+	}
+
+	if p.Octave < -2 || p.Octave > 8 {
+		return false
+	}
+
+	return true
+}
+
+// Returns the string value for a pitch by its index
+func pitchName(pitch_index int, sharp bool) (string, error) {
+	if pitch_index < 0 {
+		return "", fmt.Errorf("Invalid pitch index: %d. Expected a positive integer.", pitch_index)
+	}
+
+	if sharp {
+		return sharp_notes[pitch_index%12], nil
+	}
+	return flat_notes[pitch_index%12], nil
+}
+
+func parsePitch(text string) (pitch Pitch, err error) {
+	r := regexp.MustCompile(`(?i)^(?P<class>[a-g])(?P<accidental>[b#]{0,2})(?P<octave>-?[0-9]+)?$`)
+
+	if !r.MatchString(text) {
+		return pitch, fmt.Errorf("Invalid pitch: \"%s\"", text)
+	}
+
+	group_matches := r.FindStringSubmatch(text)
+	group_names := r.SubexpNames()
+	for idx, match := range group_matches {
+		group_name := group_names[idx]
+		switch group_name {
+		case "class":
+		case "accidental":
+			pitch.Class += match
+			break
+		case "octave":
+			octave, err := strconv.ParseInt(match, 10, 32)
+			pitch.Octave = int(octave)
+			if err != nil {
+				return pitch, err
+			}
+			break
+		default:
+			break
+		}
+	}
+	if !pitch.validatePitch() {
+		return pitch, fmt.Errorf("Pitch is not valid")
+	}
+	return pitch, err
+}
+
+// Gets the index value for a pitch string.
+//
+// E.g., c -> 0, c# -> 1, etc.
+//
+// Returns an error when the pitch string is not valid/known.
+func pitchIndex(pitch_name string) (int, error) {
+	valid_pitch_regex := regexp.MustCompile(`(?i)^[a-g][b#]{0,2}?$`)
+	if !valid_pitch_regex.MatchString(pitch_name) {
+		return -1, fmt.Errorf("Unexpected pitch name: \"%s\"", pitch_name)
+	}
+
+	pitch_name = strings.ToLower(pitch_name) // Make everything lower case for consistency
+
+	base := unicode.ToUpper(rune(pitch_name[0])) // Note pitch class denoted as a single upper-case letter
+	index := naturals[base]
+	for _, c := range pitch_name[1:] {
+		if c == 'b' {
+			index = (index + 11) % 12 // Wrap-around subtraction
+			continue
+		}
+		index = (index + 1) % 12 // Wrap-around addition
+	}
+
+	return index, nil
+}
+
+// Get the interval name between two pitches (given as string names)
+func Interval(pitch1, pitch2 string) (string, error) {
+	pitch1_index, err := pitchIndex(pitch1)
+	if err != nil {
+		return "", err
+	}
+	pitch2_index, err := pitchIndex(pitch2)
+	if err != nil {
+		return "", err
+	}
+
+	interval_distance := pitch2_index - pitch1_index
 
 	if interval_distance < 0 {
 		// Invert the interval?
@@ -37,7 +186,5 @@ func IntervalName(interval_distance int) (string, error) {
 	if !exists {
 		return "", fmt.Errorf("Unknown interval distance: %d", interval_distance)
 	}
-
-	fmt.Printf("%d = %s\n", interval_distance, name)
 	return name, nil
 }
--- pkg/app/interval_test.go
+++ pkg/app/interval_test.go
@@ -4,27 +4,155 @@ import (
 	"testing"
 )
 
-func TestDistance(t *testing.T) {
-	distance := IntervalDistance(1, 2)
-	_, err := IntervalName(distance)
+func TestInterval(t *testing.T) {
+	pitch1 := "c"
+	pitch2 := "c#"
+	expected := interval_names[1]
+	result, err := Interval(pitch1, pitch2)
 	if err != nil {
 		t.Error(err)
+	}
+	if expected != result {
+		t.Errorf("Interval(\"%s\", \"%s\") = \"%s\", expected: \"%s\"\n", pitch1, pitch2, result, expected)
 	}
 
-	distance = IntervalDistance(2, 1)
-	_, err = IntervalName(distance)
+	pitch1 = "e"
+	pitch2 = "b"
+	expected = interval_names[7]
+	result, err = Interval(pitch1, pitch2)
 	if err != nil {
 		t.Error(err)
+	}
+	if expected != result {
+		t.Errorf("Interval(\"%s\", \"%s\") = \"%s\", expected: \"%s\"\n", pitch1, pitch2, result, expected)
+	}
+	
+	_, err = parsePitch("c#-1")
+	if err != nil {
+		t.Error(err)
+	}
+	_, err = parsePitch("c##-2")
+	if err != nil {
+		t.Error(err)
+	}
+	_, err = parsePitch("c10")
+	if err != nil {
+		t.Error(err)
+	}
+	_, err = parsePitch("a")
+	if err != nil {
+		t.Error(err)
+	}
+	_, err = parsePitch("-")
+	if err != nil {
+		t.Error(err)
+	}
+	_, err = parsePitch("a-")
+	if err != nil {
+		t.Error(err)
+	}
+	t.Fail()
+}
+
+func TestPitchIndex(t *testing.T) {
+	pitch := "c"
+	index, err := pitchIndex(pitch)
+	expected_index := 0
+	if err != nil {
+		t.Error(err)
+	}
+	if index != expected_index {
+		t.Errorf("pitchIndex(\"%s\") = %d, expected: %d\n", pitch, index, expected_index)
+	}
+	pitch = "C"
+	index, err = pitchIndex(pitch)
+	expected_index = 0
+	if err != nil {
+		t.Error(err)
+	}
+	if index != expected_index {
+		t.Errorf("pitchIndex(\"%s\") = %d, expected: %d\n", pitch, index, expected_index)
 	}
 
-	distance = IntervalDistance(0, 5)
-	_, err = IntervalName(distance)
+	pitch = "cbb"
+	index, err = pitchIndex(pitch)
+	expected_index = 10
 	if err != nil {
 		t.Error(err)
 	}
-	distance = IntervalDistance(5, 0)
-	_, err = IntervalName(distance)
+	if index != expected_index {
+		t.Errorf("pitchIndex(\"%s\") = %d, expected: %d\n", pitch, index, expected_index)
+	}
+	pitch = "c##"
+	index, err = pitchIndex(pitch)
+	expected_index = 2
 	if err != nil {
 		t.Error(err)
+	}
+	if index != expected_index {
+		t.Errorf("pitchIndex(\"%s\") = %d, expected: %d\n", pitch, index, expected_index)
+	}
+	pitch = "b##"
+	index, err = pitchIndex(pitch)
+	expected_index = 1
+	if err != nil {
+		t.Error(err)
+	}
+	if index != expected_index {
+		t.Errorf("pitchIndex(\"%s\") = %d, expected: %d\n", pitch, index, expected_index)
+	}
+}
+
+func TestPitchValidation(t *testing.T) {
+	// Invalid inputs fail validation
+	pitch := "h"
+	_, err := pitchIndex(pitch)
+	if err == nil {
+		t.Errorf("Regex failed to validate invalid pitch: \"%s\"", pitch)
+	}
+	pitch = "cbbb"
+	_, err = pitchIndex(pitch)
+	if err == nil {
+		t.Error("Validation failed to catch too many flats (b's)")
+	}
+	pitch = "c###"
+	_, err = pitchIndex(pitch)
+	if err == nil {
+		t.Error("Validation failed to catch too many sharps (#'s)")
+	}
+
+	// Valid inputs pass validation
+	for i := int('a'); i <= int('g'); i++ {
+		// Naturals
+		c := string(rune(i))
+		_, err = pitchIndex(c)
+		if err != nil {
+			t.Errorf("Validation failed valid pitch: \"%s\"", pitch)
+		}
+		// Sharps
+		sharp := c + "#"
+		_, err = pitchIndex(sharp)
+		if err != nil {
+			t.Errorf("Validation failed valid pitch: \"%s\"", sharp)
+		}
+		// Double sharps
+		sharp += "#"
+		_, err = pitchIndex(sharp)
+		if err != nil {
+			t.Errorf("Validation failed valid pitch: \"%s\"", sharp)
+		}
+
+		// Flats
+		flat := c + "b"
+		_, err = pitchIndex(flat)
+		if err != nil {
+			t.Errorf("Validation failed valid pitch: \"%s\"", flat)
+		}
+		// Double flats
+		flat += "b"
+		_, err = pitchIndex(flat)
+		if err != nil {
+			t.Errorf("Validation failed valid pitch: \"%s\"", flat)
+		}
 	}
 }
